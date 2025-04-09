48e278f6223b8876e4cfc6526e8ab1c793ba66da
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-09 22:13:17 +0200

    añadir css

--- Practica/generadorExcusasV2.js
+++ Practica/generadorExcusasV2.js
@@ -1,11 +1,16 @@
 const excusa = document.getElementById("excusa");
+let enseñar = document.getElementById("enseñar")
 
 const QUIENES = ["Q1", "Q2", "Q3"];
 const ACCIONES = ["k1", "k2", "k3"];
 const TIEMPOS = ["ayer", "hoy", "mañana"];
 const contadorElement = document.getElementById("contador"); 
 
+let favoritas = []
+
 let contador = 0;
+
+
 
 
 function opcionAleatoria(array) {
