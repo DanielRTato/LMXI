000d0bd2e39166a7ac03476de45a776d92ab9a3a
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-09 10:20:37 +0200

    ejemlpo interruptor

--- Botones/botones.js
+++ Botones/botones.js
@@ -7,3 +7,15 @@ function emitSound() {
     audio.currentTime = 0;
     audio.play();
 }
+
+//ejemplo de interruptor
+
+function mostrarImagenClicada() { 
+    const imagen = document.querySelector("img.clickada")
+
+    if (imagen.style.dispplay ==="block"){
+        imagen.style.dispplay == "none"
+    } else{
+        imagen.style.dispplay = "block"
+    }
+}
