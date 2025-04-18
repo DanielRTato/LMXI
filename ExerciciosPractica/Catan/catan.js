100a1d6bdfa321d3767a9464af814c4c3720950f
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-18 14:17:54 +0200

    Catan html y funcoiun para tirar dados

---
+++ ExerciciosPractica/Catan/catan.js
@@ -0,0 +1,10 @@
+let manoBranco = [];
+let manoVermello = [];
+let manoAzul = [];
+let manoAmarelo = [];
+
+function tirarDados(){
+    let dado = 0;
+    dado = Math.floor(Math.random()*12) + 1;
+    console.log(dado)
+}
