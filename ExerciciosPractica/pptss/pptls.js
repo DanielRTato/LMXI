3e896d26dbf0033d3764339f811481d614a1b030
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-19 03:01:42 +0200

    pptlds

---
+++ ExerciciosPractica/pptss/pptls.js
@@ -0,0 +1,36 @@
+let pick1 = "";
+let pick2;
+
+// eleccion del player1
+document.getElementById("rock").onclick = function(){
+    pick1 = "rock";
+}
+document.getElementById("Scissors").onclick = function(){
+    pick1 = "scissors";
+}
+document.getElementById("Spock").onclick = function(){
+    pick1 = "spock";
+}
+document.getElementById("Lizard").onclick = function(){
+    pick1 = "lizard";
+}
+document.getElementById("paper").onclick = function(){
+    pick1 = "paper";
+}
+
+// eleccion del player2
+document.getElementById("rock").onclick = function(){
+    pick2 = "rock";
+}
+document.getElementById("Scissors").onclick = function(){
+    pick2 = "scissors";
+}
+document.getElementById("Spock").onclick = function(){
+    pick2 = "spock";
+}
+document.getElementById("Lizard").onclick = function(){
+    pick2 = "lizard";
+}
+document.getElementById("paper").onclick = function(){
+    pick2 = "paper";
+}
