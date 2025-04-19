266c153f4ff396303ca3537137534e40cb09063b
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-19 14:30:20 +0200

    logica de juego

--- ExerciciosPractica/pptss/pptls.js
+++ ExerciciosPractica/pptss/pptls.js
@@ -34,3 +34,43 @@ document.getElementById("Lizard").onclick = function(){
 document.getElementById("paper").onclick = function(){
     pick2 = "paper";
 }
+
+document.getElementById("play").onclick = function{
+    if (pick1 && pick2){
+        if (pick1==rock & pick2==lizard) {
+            document.getElementById("resultado").innerHTML += "Piedra aplasta lagarto\n Gaña o XOGADOR 1";
+        }else if(pick1==rock & pick2 == scissors) { 
+            document.getElementById("resultado").innerHTML += "Piedra aplasta tijera\n Gaña o XOGADOR 1"
+        }
+        
+        if(pick1==lizard & pick2==spock){
+            document.getElementById("resultado").innerHTML += "lagarto envenena Spock\n Gaña o XOGADOR 1"
+        }else if(pick1==lizard & pick2==paper){
+            document.getElementById("resultado").innerHTML += "lagarto devora papel\n Gaña o XOGADOR 1"
+        }
+        
+        if(pick1=="spock" & pick2=="scissors") {
+            document.getElementById("resultado").innerHTML += "Spock rompe tijera\n Gaña o XOGADOR 1"
+        }else if(pick1 =="spock" & pick2=="rock") {
+            document.getElementById("resultado").innerHTML += "Spock vaporiza piedra\n Gaña o XOGADOR 1"
+        }
+        
+        if (pick1=="scissors" & pick2 == "paper"){
+            document.getElementById("resultado").innerHTML += "Tijera cortal papel\n Gaña o XOGADOR 1"
+        }else if(pick1=="scissors" & pick2 == "lizard") {
+            document.getElementById("resultado").innerHTML += "tijera decapita lagarto\n Gaña o XOGADOR 1"
+        }
+        
+        if (pick1 =="paper" & pick2 =="rock") { 
+            document.getElementById("resultado").innerHTML += "Papel envuelve piedra\n Gaña o XOGADOR 1"
+        }else if(pick1=="paper" & pick2 == "spock") {
+            document.getElementById("resultado").innerHTML += "Papel desautoriza Spock\n Gaña o XOGADOR 1"
+        }
+        else {
+            document.getElementById("resultado").innerHTML += "Gana o XOGADOR 2";
+        }
+    }
+
+
+}
+
