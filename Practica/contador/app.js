2115a11f7a79155a53e6d21769c7d415224ecc1e
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-29 19:38:15 +0200

    contadorv2

--- Practica/contador/app.js
+++ Practica/contador/app.js
@@ -1,3 +1,25 @@
-let count = 0;
+let contador = 0;
+const aumentar = document.getElementById("aumentar");
+const restar = document.getElementById("restar");
+const reset = document.getElementById("reset");
+const resultado = document.getElementById("valor");
 
-const value = document.querySelector('#value')
+function aumenta() {
+    contador++;
+    resultado.innerHTML = contador;
+    resultado.style.color = "green";
+    
+}
+
+function resta() {
+    contador--;
+    resultado.innerHTML = contador;
+    resultado.style.color = "red";
+
+} 
+
+function resetear() {
+    contador = 0;
+    resultado.innerHTML = contador;
+    resultado.style.color = "black";
+}
